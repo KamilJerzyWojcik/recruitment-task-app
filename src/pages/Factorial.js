2f688fb3c929d76bb29a41e817ef2f96ce7ef3cc
Kamil Wojcik <kamil.jerzy.wojcik@gmail.com>
2022-05-06 12:49:10 +0200

    init pages

---
+++ src/pages/Factorial.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Factorial = () => {
+    return <h1>Factorial page</h1>
+};
+
+export default Factorial;
---
+++ src/pages/Github.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Github = () => {
+    return <h1>Github page</h1>
+};
+
+export default Github;
