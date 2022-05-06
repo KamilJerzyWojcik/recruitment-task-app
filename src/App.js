2d7618d1a847ae60837775b152a44316710433a9
Kamil Wojcik <kamil.jerzy.wojcik@gmail.com>
2022-05-06 12:49:26 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Github from "./pages/Github";
+import About from "./pages/About";
+import Factorial from "./pages/Factorial";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="*" element={<Navigate replace to="/github" />}/>
+      <Route index path="/github" element={<Github />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/factorial" element={<Factorial />} />
+    </Routes>
   );
 }
 
